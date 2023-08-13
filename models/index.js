f199bfccdcd1271ee6ce91700052167d0e6ba5e6
Joel Getzke <joelcode2023@gmail.com>
2023-08-13 10:40:48 -0500

    comments on the model index for readability

--- models/index.js
+++ models/index.js
@@ -2,27 +2,32 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-
+// makes it so the user can own multiple posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 })
 
+// makes it so the user can have multiple comments
 User.hasMany(Comment, {
     foreignKey: 'user_id'
 })
 
+// Shows who the post belongs to.
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
+// makes it so posts can have multiple comments on it
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 })
 
+// Shows who the comment belongs to.
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
+// Makes it so the comment will refer to that particular post.
 Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 })
